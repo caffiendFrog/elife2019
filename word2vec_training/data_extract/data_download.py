1b57cd39aee90e50146f560c723819ea35d1a7ff
michael <m.owonibi@elifesciences.org>
2019-09-04 16:17:06 +0100

    initial commit word_vec training

--- word2vec_training/data_extract/data_download.py
+++ word2vec_training/data_extract/data_download.py
@@ -4,11 +4,33 @@ from config import EQUIPMENT_FILE_NAME
 import nltk
 from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
-from nltk.util import ngrams
 from nltk import everygrams
+from nltk.corpus import stopwords
+
+
+from glob import glob
+
+
+
 
 nltk.download('wordnet')
 nltk.download('punkt')
+nltk.download('stopwords')
+
+stop_words = stopwords.words('english')
+stop_words.append("'")
+stop_words.append("''")
+stop_words.append(".")
+stop_words.append(")")
+stop_words.append("(")
+stop_words.append("&")
+
+stop_words.append("system")
+stop_words.append("analyzer")
+stop_words.append("function")
+stop_words.append('documentation')
+#stop_words.append("science")
+#stop_words.append("scientific")
 
 
 def load_equipment_names () :
@@ -16,17 +38,29 @@ def load_equipment_names () :
         all_ngram = list()
         for line in equipment_files:
             all_ngram = list(set(all_ngram + tokenize_lemmatize(line)))
-        return  all_ngram
+        return all_ngram
 
 
 def tokenize_lemmatize(sentence) :
+    sentence = sentence.lower().strip('"')
     lemmatizer = WordNetLemmatizer()
     tokenized_list = nltk.word_tokenize(sentence)
+    tokenized_list = [word for word in tokenized_list if word not in stop_words]
+
     lemmatized_word_list = [lemmatizer.lemmatize(w) for w in tokenized_list]
-    every_gramm_list = list(everygrams(lemmatized_word_list, 1, 3))
+    every_gramm_list = list(everygrams(lemmatized_word_list, 2, 4))
+    #print(every_gramm_list)
     return [' '.join(gram) for gram in every_gramm_list]
 
 
+def get_all_xml_files():
+    files = []
+    start_dir = os.getcwd()
+    pattern = "*."
+
+    for dir, _, _ in os.walk(start_dir):
+        files.extend(glob(os.path.join(dir, pattern)))
+    return None
 
 
 
