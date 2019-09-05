90d6e3a35e73603e2264130484fe3eacac2b53dd
michael <m.owonibi@elifesciences.org>
2019-09-05 13:36:03 +0100

    uses word2vec to analyse body of text to get possible equipment names

--- word2vec_training/main.py
+++ word2vec_training/main.py
@@ -1,12 +1,59 @@
-from data_extract.data_download import load_equipment_names, get_all_xml_file_names,preprocess_and_persist_string, process_all_files
-from train_word2vec.train import
-
+#from data_extract.data_download import load_equipment_names, get_all_xml_file_names,preprocess_and_persist_string, process_all_files
+from text_analyser.use_word2vec import load_all_equipment_names, get_most_similar, get_distance, compare_noun_phrase_to_equipment_name_list
+import spacy
 
 if __name__ == "__main__":
+    print(get_distance('mass', 'spectrometer'))
 
-    ngram_data = load_equipment_names()
-    all_xml_file_name = get_all_xml_file_names()
-    process_all_files(all_xml_file_name)
+    print(get_distance('argon', 'ion'))
+    print(get_distance('microscope', 'microscopy'))
+    print(get_distance('microscope', 'microscopes'))
+    tokenized_instruments_name = load_all_equipment_names()
+
+    print(compare_noun_phrase_to_equipment_name_list(['organization','fellow'], tokenized_instruments_name))
+
+
+
+
+def something():
+    tokenized_instruments_name = load_all_equipment_names()
+
+    for x in tokenized_instruments_name[4 :10]:
+        for i in  x:
+            print(i)
+            print(get_most_similar(i))
+            print("")
+
+
+'''
+def main():
+
+    #ngram_data = load_equipment_names()
+    #all_xml_file_name = get_all_xml_file_names()
+    #process_all_files(all_xml_file_name)
+
+    print(get_most_similar('thermometer'))
+    print("")
+    print(get_most_similar('bioanalyzer'))
+    print("")
+    print("")
+
+    print(get_most_similar('cellprofiler'))
+    print("")
+    print("")
+
+    print(get_most_similar('thermocycler'))
+    print("")
+
+    print("")
+
+    print(get_most_similar('centrifugal concentrator'))
+    print("")
+
+    print("")
+
+    print(get_most_similar('thermal cycler'))
+    print("")
 
 
 
@@ -17,3 +64,4 @@ if __name__ == "__main__":
 
     #print(ngram_data)
 
+'''
---
+++ word2vec_training/text_analyser/use_word2vec.py
@@ -0,0 +1,65 @@
+from gensim.models import KeyedVectors
+from config import WORD2VEC_MODEL_LOCATION, EQUIPMENT_FILE_NAME, SINGLE_WORD_DISTANCE_THRESHOLD, SINGLE_PHRASE_DISTANCE_THRESHOLD
+import spacy
+import copy
+from spacy.tokenizer import Tokenizer
+from nltk.corpus import stopwords
+
+
+from spacy.util import compile_prefix_regex, compile_infix_regex, compile_suffix_regex
+
+# Load vectors directly from the file
+
+
+model =  KeyedVectors.load_word2vec_format(WORD2VEC_MODEL_LOCATION, binary=True)
+nlp = spacy.load("en")
+stop_words = stopwords.words('english')
+stop_words.extend(["'"'', "''", ".",")", "(", "&","\n", '', '_', '\\', '/', '-'])
+
+def get_most_similar(word):
+    try:
+        return model.most_similar(word)
+    except:
+        return
+
+def get_distance(word1, word2 ):
+    return model.distance(word1, word2)
+
+
+def load_all_equipment_names():
+    tokenized_equimpment = []
+    for file in EQUIPMENT_FILE_NAME :
+        with open(file) as equipment_files:
+            for line in equipment_files:
+                sentence = nlp(line)
+                tokenized_list = [word.text.strip().lower() for word in sentence if word.text.strip().lower() not in stop_words]
+                tokenized_equimpment.append(tokenized_list)
+    return tokenized_equimpment
+
+
+def get_all_noun_phrases(document_text):
+    doc = nlp(document_text)
+    try:
+        noun_phrases = []
+        for np in doc.noun_chunks:
+            noun_phrases.append(np)
+        return noun_phrases
+    except:
+        return document_text
+
+
+def compare_noun_phrase_to_equipment_name_list(word_list, all_equipments,):
+    for equipment in all_equipments:
+        distance_list = []
+        for word in equipment :
+            for new_word in word_list:
+                try:
+                    dist =get_distance(word, new_word)
+                    if dist < SINGLE_WORD_DISTANCE_THRESHOLD:
+                        distance_list.append(dist)
+                        continue
+                except:
+                    continue
+        if len(distance_list)> 0 and sum(distance_list)/len(distance_list) < SINGLE_PHRASE_DISTANCE_THRESHOLD:
+            return True
+    return  False
