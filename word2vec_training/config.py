a4c2ac27afdc0e3bccaa679375614e75f93480c7
michael <m.owonibi@elifesciences.org>
2019-09-05 09:31:34 +0100

    initial commit word_vec training

--- word2vec_training/config.py
+++ word2vec_training/config.py
@@ -1,2 +1,5 @@
 EQUIPMENT_FILE_NAME = "/home/michael/PycharmProjects/elife2019/alaska-instruments"
 TRAINING_DATA_DIR = '/home/michael/pubmed/'
+ARTICLE_BODY = '/home/michael/article_body/'
+PREPROCESSED_ARTICLE_BODY = '/home/michael/preprocessed_article_body/'
+
