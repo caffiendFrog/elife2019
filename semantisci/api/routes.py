17c54528c29e734b2b5ec3b59d6d5208a4cab5de
sophia k cheng <sophia@addgene.org>
2019-09-05 12:19:41 +0100

    Converting ui to a form. Added message area to html

--- semantisci/api/routes.py
+++ semantisci/api/routes.py
@@ -1,20 +1,34 @@
 from flask import Flask, request, current_app, render_template
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, TextAreaField, SelectMultipleField, SubmitField
 import json
 import requests
 import os
 import random
 
 from . import api
-from .utilities import get_random_article
+from .utilities import get_random_article, make_choices
 
 @api.route('/abstract/', methods = ['GET'])
 def display_article():
     id, article = get_random_article()
     text = article.get('text', None)
     candidates = article.get('candidates', None)
-    return render_template('index.html', id = id, message = text, candidates = candidates)
+
+    form = CandidateValidationForm()
+    form.id = id
+    form.candidates.choices = make_choices(candidates)
+    form.text = text
+    return render_template('index.html', form=form)
     
 
 @api.route('/validate', methods = ['POST'])
 def validate_equipment():
     pass
+
+
+class CandidateValidationForm(FlaskForm):
+    id = StringField('Article ID', render_kw={'readonly': True})
+    text = TextAreaField('Article excerpt')
+    candidates = SelectMultipleField('Candidates')
+    submit = SubmitField('Confirm')
