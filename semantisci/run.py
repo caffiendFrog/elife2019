a41c89de2710515b04c23594bdd57a16266bc7f8
Wambui Karuga <wambui@karuga.xyz>
2019-09-05 10:26:27 +0100

    Initial Flask api commit.

---
+++ semantisci/run.py
@@ -0,0 +1,17 @@
+import sys
+from api import api
+from flask import Flask
+from flask_cors import CORS, cross_origin
+
+def setup():
+    application = Flask(__name__)
+    application.register_blueprint(api, url_prefix = '/api/')
+    CORS(application)
+    return application
+
+app = setup()
+
+if __name__ == '__main__':
+#    app = setup()
+#    app.config['file'] = sys.argv[1]
+    app.run(debug = True, port = 5000)
